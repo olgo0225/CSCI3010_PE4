234df3cdf972bbe7716704566345f90e97dc8144
Vienna Wong <viwo3022@colorado.edu>
2022-02-09 14:47:39 -0700

    Added main. Reformatted PrintBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -25,18 +25,40 @@ void DisplayBoard(vector<int> board)
         {
             cout << endl;
         }
-        cout << board[i];
+        if (board[i] == -1)
+        {
+            cout << "[]";
+        }
+        else
+        {
+            cout << board[i] << " ";
+        }
     }
+}
+
+vector<int> GetPlayerChoice()
+{
+    vector<int> choices;
+    cout << endl;
+    cout << "Input row of desired location:" << endl;
+    int choice_x;
+    cin >> choice_x;
+    choices.push_back(choice_x);
+    cout << "Input column of desired location:" << endl;
+    int choice_y;
+    cin >> choice_y;
+    choices.push_back(choice_y);
+    return choices;
 }
 
 int PosToIndex(vector<int> pos)
 {
-    int x = pos[0];
-    int y = pos[1];
+    int x = pos[1];
+    int y = pos[0];
     int index = -1;
     if (y == 2)
     {
-        index = x;
+        index = x + 6;
     }
     else if (y == 1)
     {
@@ -44,7 +66,7 @@ int PosToIndex(vector<int> pos)
     }
     else if (y == 0)
     {
-        index = x + 6;
+        index = x;
     }
     else
     {
@@ -53,16 +75,30 @@ int PosToIndex(vector<int> pos)
     return index;
 }
 
-void PlaceMarker(vector<int> pos, int marker, vector<int> b)
+vector<int> PlaceMarker(vector<int> pos, int marker, vector<int> b)
 {
-    int x = pos[0];
-    int y = pos[1];
+    int x = pos[1];
+    int y = pos[0];
     int index = PosToIndex(pos);
     b[index] = marker;
+    return b;
 }
 
 int main()
 {
     vector<int> ticTacToe_board = CreateBoard();
+    for (int i = 0; i < 9; i++)
+    {
+        DisplayBoard(ticTacToe_board);
+        vector<int> turn = GetPlayerChoice();
+        if (i % 2 == 0)
+        {
+            ticTacToe_board = PlaceMarker(turn, 0, ticTacToe_board);
+        }
+        else
+        {
+            ticTacToe_board = PlaceMarker(turn, 1, ticTacToe_board);
+        }
+    }
     DisplayBoard(ticTacToe_board);
 }
